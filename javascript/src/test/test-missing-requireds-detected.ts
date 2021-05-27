85d20a9196400e3ad69d3b4efa6585b0f659cdc3
Meinte Boersma <meinte.boersma@gmail.com>
2021-05-27 13:34:58 +0200

    (fix small stuff)

--- javascript/src/test/test-missing-requireds-detected.ts
+++ javascript/src/test/test-missing-requireds-detected.ts
@@ -1,34 +1,38 @@
+import { ErrorObject } from "ajv"
 const { equal } = require("chai").assert
 
 import { schemaValidationErrorsFor } from "../validator"
 
 
-describe.only("missing required fields are detected by schema validator", () => {
+describe("missing required fields are detected by schema validator", () => {
+
+    const missingRequiredPropertyErrors = (errors: ErrorObject[]) =>
+        errors.filter((error) => error.message?.startsWith("must have required property "))
 
     it("empty DGC", () => {
         const errors = schemaValidationErrorsFor({})
-        equal(errors.filter((error) => error.message?.startsWith("must have required property ")).length, 3)
+        equal(missingRequiredPropertyErrors(errors).length, 3)
     })
 
     it("empty r-event", () => {
         const errors = schemaValidationErrorsFor({
             r: [ {} ]
         })
-        equal(errors.filter((error) => error.message?.startsWith("must have required property ")).length, 3 + 7)
+        equal(missingRequiredPropertyErrors(errors).length, 3 + 7)
     })
 
     it("empty t-event", () => {
         const errors = schemaValidationErrorsFor({
             t: [ {} ]
         })
-        equal(errors.filter((error) => error.message?.startsWith("must have required property ")).length, 3 + 8)
+        equal(missingRequiredPropertyErrors(errors).length, 3 + 8)
     })
 
     it("empty v-event", () => {
         const errors = schemaValidationErrorsFor({
             v: [ {} ]
         })
-        equal(errors.filter((error) => error.message?.startsWith("must have required property ")).length, 3 + 10)
+        equal(missingRequiredPropertyErrors(errors).length, 3 + 10)
     })
 
 })
