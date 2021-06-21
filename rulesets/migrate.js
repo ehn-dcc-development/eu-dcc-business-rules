7aa12724f97a7463f81be991f297a39e0386b1ce
Meinte Boersma <meinte.boersma@gmail.com>
2021-06-21 20:25:10 +0200

    implement migration from existing rule (set) format (including tests) to new one in dgc-business-rules-testdata repo
    Squashed commit of the following:
    
    commit 14ffa5119ef7364ccabdcc2dc54b49f80a1a3b63
    Author: Meinte Boersma <meinte.boersma@gmail.com>
    Date:   Mon Jun 21 20:24:23 2021 +0200
    
        (remove unneeded Git ignore)
    
    commit 80f8401d86f5e076a17f82da4b039a19d3845610
    Author: Meinte Boersma <meinte.boersma@gmail.com>
    Date:   Mon Jun 21 16:05:41 2021 +0200
    
        put tests one dir. level deeper
    
    commit d0d80b74c8e983b87b4bf740c9ef9ca8722ea8a0
    Author: Meinte Boersma <meinte.boersma@gmail.com>
    Date:   Mon Jun 21 14:52:59 2021 +0200
    
        migrate tests as well
    
    commit 16594257284e07d6ee360e52f819c52a697d8bf8
    Author: Meinte Boersma <meinte.boersma@gmail.com>
    Date:   Mon Jun 21 14:31:39 2021 +0200
    
        fix migration for new structuring
    
    commit b1f317e817666f2bff161f3d3852a4c4e6fe5a54
    Author: Meinte Boersma <meinte.boersma@gmail.com>
    Date:   Sun Jun 20 23:28:32 2021 +0200
    
        implement migration from “old” format to official one

---
+++ rulesets/migrate.js
@@ -0,0 +1,98 @@
+const { existsSync, mkdirSync, readFileSync, readdirSync, writeFileSync } = require("fs")
+const { join } = require("path")
+
+
+const { affectedFields } = require("../certlogic/certlogic-validation/dist/affected-fields")
+
+
+const readJson = (path) => JSON.parse(readFileSync(path, "utf8"))
+const writeJson = (path, data) => writeFileSync(path, JSON.stringify(data, null, 2), "utf8")
+
+const rulesRepoPath = `../../dgc-business-rules-testdata`
+
+const ruleType = (ruleId) => {
+    const match = ruleId.match(/^(\w{2})-(\w{2})-(\d{4})$/)
+    if (!match) {
+        throw new Error(`rule ID "${ruleId}" doesn't conform to regex`)
+    }
+    switch (match[1]) {
+        case "GR": return "General"
+        case "RR": return "Recovery"
+        case "TR": return "Test"
+        case "VR": return "Vaccination"
+    }
+}
+
+const migrateRule = (rule, msId) => ({
+    "Identifier": rule.id,
+    "Type": "Acceptance",
+    "Country": msId,
+    "Version": "1.0.0",
+    "SchemaVersion": "1.0.0",
+    "Engine": "CERTLOGIC",
+    "EngineVersion": "0.7.5",
+    "CertificateType": ruleType(rule.id),
+    "Description": [
+        {
+            "lang": "en",
+            "desc": rule.description
+        }
+    ],
+    "ValidFrom": "2021-06-01T00:00:00Z",
+    "ValidTo": "2030-06-01T00:00:00Z",
+    "AffectedFields": affectedFields(rule.logic)
+        .filter((fieldName) => fieldName.startsWith("payload."))
+        .map((fieldName) => fieldName.substring("payload.".length))
+        .sort(),
+    "Logic": rule.logic
+})
+
+const compressedValueSets = readJson("../rules-runner/resources/valueSets.json")
+
+const migrate = (msId, rsId) => {
+    const ruleSetDstPath = join(rulesRepoPath, msId)
+    mkdirSync(ruleSetDstPath, { recursive: true })
+
+    const ruleSetSrcPath = join(msId, `${rsId}.json`)
+    readJson(ruleSetSrcPath).forEach((rule) => {
+        const ruleDstPath = join(ruleSetDstPath, rule.id)
+        if (!existsSync(ruleDstPath)) {
+            mkdirSync(ruleDstPath)
+        }
+        writeJson(join(ruleDstPath, `rule.json`), migrateRule(rule, msId))
+    })
+
+    const rulesTestsSrcPath = join(msId, "tests")
+    const tests = readdirSync(rulesTestsSrcPath)
+        .filter((path) => path.endsWith(".json"))
+
+    tests.forEach((ruleTestsSrcPath) => {
+        const ruleId = ruleTestsSrcPath.substring(0, ruleTestsSrcPath.length - ".json".length)
+        const ruleDstPath = join(ruleSetDstPath, ruleId)
+        const ruleTestsDstPath = join(ruleDstPath, "tests")
+        if (!existsSync(ruleTestsDstPath)) {
+            mkdirSync(ruleTestsDstPath)
+        }
+
+        const ruleTests = readJson(join(rulesTestsSrcPath, ruleTestsSrcPath))
+        ruleTests.forEach((ruleTest, index) => {
+            writeJson(
+                join(ruleTestsDstPath, `test${("" + (index + 1)).padStart(3, "0")}.json`),
+                {
+                    name: ruleTest.name,
+                    payload: ruleTest.payload,
+                    expected: ruleTest.expected,
+                    external: {
+                        validationClock: ruleTest.validationClock,
+                        valueSets: compressedValueSets
+                    }
+                }
+            )
+        })
+    })
+}
+
+migrate("EU", "template-ruleset")
+migrate("NL", "NL-specific-ruleset")
+migrate("FI", "FI-specific-ruleset")
+
