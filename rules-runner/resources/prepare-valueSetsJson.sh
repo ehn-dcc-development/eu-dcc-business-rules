56e78d7564bb2c15f6baace5c564c9ebd3a9f3d8
Christian <christian.kollmann@a-sit.at>
2021-06-30 13:37:46 +0200

    Update prepare-valueSetsJson.sh
    Replace bash syntax `[[` with sh syntax `[`

--- rules-runner/resources/prepare-valueSetsJson.sh
+++ rules-runner/resources/prepare-valueSetsJson.sh
@@ -1,7 +1,7 @@
 # TODO  check whether ../../ehn-dcc-valuesets/*.json exists, and is non-empty
 # TODO  check whether jq is installed
 VALUESETS_PATH=../../../ehn-dcc-valuesets
-if [[ -d "$VALUESETS_PATH" ]]
+if [ -d "$VALUESETS_PATH" ]
 then
   jq --slurp 'map( { (.valueSetId): .valueSetValues|keys }) | add' $VALUESETS_PATH/*.json > valueSets.json
 else
